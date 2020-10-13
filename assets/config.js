a1484ba0d859745ea0addd46ad599101d6ddea2e
Ana Betancurt Leiva <anitabetancourt@hotmail.com>
2020-10-13 16:14:21 -0500

    actualizado

--- assets/config.js
+++ assets/config.js
@@ -7,18 +7,18 @@ const persona = {
 	profesion: "Desarrollador junior",
 	correo: "anitabetancourt@hotmail.com",
 	telefono: "+57 3006454464",
-	perfil: "se destaca por la organizacion en el cumplimiento de labores y requerimientos que se exigen,capaz de liderar y solucionar ptoblemas rapidamente. posee muchos valores,tiene la facilidad para la comunicacion y el buen entendimiento desde la comprension y la empatia con las personas que la rodean.",
+	perfil: "se destaca por la organizacion en el cumplimiento de labores y requerimientos que se exigen,capaz de liderar y solucionar problemas rapidamente. posee muchos valores, tiene la facilidad para la comunicacion y el buen entendimiento desde la comprension y la empatia con las personas que la rodean.",
 	habilidades: {
 		comunicativas:['organizada, ','proactiva, ','Actitud Positiva, ','Tolerancia, ','Respeto, ','honestidad, ','Orden, ','Apoyo y compernsión'],
 		gestion: ['capaz de liderar, ','facilidad para desenvolver en trabajo en equipo, ','Responsabilidad, ','habilidad para absorber conocimientos teoricos y practicos, ','analisis de Decisiones'],
-		tecnicas: ['esteticista y cosmetologa, ','estudiante ']
+		tecnicas: ['esteticista y cosmetologa, ','estudiante de ADSI']
 	},
 	hobbies: ['Escuchar Música, ', 'Ver Peliculas, ', 'viajar, ', 'hacer postres']
 }
 
 // Dejar intácto a partir de aquí.
 // Constantes SENA:
-const banner = "https://3.bp.blogspot.com/-TkVKyVxdxgQ/U2kGFcBl3LI/AAAAAAAABVo/zrDM3ittaz0/s1600/Banner+Soy+Cultura+SENA+-+CTMA.png";
+const banner = "https://thumbs.dreamstime.com/z/programming-concept-idea-working-computer-coding-testing-writing-program-using-internet-different-software-set-124176396.jpg";
 // Mostramos el objeto persona en la consola:
 console.log(persona);
 
